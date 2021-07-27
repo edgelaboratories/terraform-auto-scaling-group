7bd92b837f2357f48f6770aa9473403b75ba7f0c
Jonathan Ballet <jballet@edgelab.ch>
2021-07-27 19:00:22 +0200

    export asg arn

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "name" {
   value = aws_autoscaling_group.this.name
 }
 
+output "arn" {
+  value = aws_autoscaling_group.this.arn
+}
+
 output "target_group_arn" {
   value = aws_lb_target_group.this.*.arn
 }
